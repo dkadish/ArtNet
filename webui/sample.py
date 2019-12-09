46e137ffbf03237821d041bc31f49e8cfc5668a2
Mace Ojala <maco@itu.dk>
2019-12-09 16:31:45 +0100

    Added a straightforward upload form.

--- webui/sample.py
+++ webui/sample.py
@@ -1,6 +1,6 @@
 # A sample web UI for ArtNet.
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -12,4 +12,14 @@ def hello_world():
 
 @app.route('/')
 def index():
+    return render_template('sample.html')
+
+
+@app.route('/upload_image', methods=['GET', 'POST'])
+def upload_image():
+    if request.method == 'POST':
+        image = request.files['image']
+        print(f"{image}, {image.content_length} bytes")
+        return redirect(request.url)
+
     return render_template('sample.html')
